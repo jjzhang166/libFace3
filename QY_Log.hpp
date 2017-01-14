54304d149d30e07057a30a2826422841cdddcb38
kycoder <xkycn@outlook.com>
2017-01-14 23:51:46 +0800

    delete all file

--- QY_Log.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef QY_LOG_HPP
-#define QY_LOG_HPP
-
-#include <cstdio>
-
-#define LOG_INFO(format, ...) fprintf(stderr, "[%s][%d] " format, __func__, __LINE__, ## __VA_ARGS__)
-
-#endif // QY_LOG_HPP
